ffdd5025a82c5fee287535f6d386fa4ef3724cca
Rpsmith77 <rpsmith77@gmail.com>
2019-10-01 16:34:35 -0400

    Exercise 4

---
+++ Exercise5.py
@@ -0,0 +1,9 @@
+# Ryan Smith
+# Python for Everybody Coursera
+
+# Exercise 5: Write a program which prompts the user for a Celsius temperature, convert the temperature to
+# Fahrenheit, and print out the converted temperature.
+
+celsius = float(input("Enter temperature in celsius: "))
+fahrenheit = celsius *(9 / 5) + 32
+print("In fahrenheit that is {:.2f} degrees".format(fahrenheit))
