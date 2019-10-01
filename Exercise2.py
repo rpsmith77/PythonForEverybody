68d135c169974453cc9b117e58c38c33271b85d8
Rpsmith77 <rpsmith77@gmail.com>
2019-10-01 16:20:45 -0400

    Exercise 2

---
+++ Exercise2.py
@@ -0,0 +1,8 @@
+# Ryan Smith
+# Python for Everybody Coursera
+
+# Exercise 2: Write a program that uses input to prompt a user for their
+# name and then welcomes them.
+
+name = input("Enter your name: ")
+print("Hello",name)
