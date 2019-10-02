5e1d6e6eb6c41c2590a6afff41d5d658e8eb1686
Rpsmith77 <rpsmith77@gmail.com>
2019-10-01 21:56:13 -0400

    Ch 3 Exercise 2

--- CH3_Exercise2.py
+++ CH3_Exercise2.py
@@ -12,18 +12,16 @@
 # Enter Hours: forty
 # Error, please enter numeric input
 
-while True:
-    try:
-        hours = float(input("Enter Hours: "))
-        rate =float(input("Enter Rate: "))
-        if hours <= 40:
-            pay = hours * rate
-            print("Pay: ${:.2f}".format(pay))
-        else:
-            pay = 40 * rate
-            pay += (hours - 40) * (rate * 1.5)
-            print("Pay: ${:.2f}".format(pay))
+try:
+    hours = float(input("Enter Hours: "))
+    rate =float(input("Enter Rate: "))
+    if hours <= 40:
+        pay = hours * rate
+        print("Pay: ${:.2f}".format(pay))
+    else:
+        pay = 40 * rate
+        pay += (hours - 40) * (rate * 1.5)
+        print("Pay: ${:.2f}".format(pay))
 
-    except:
-        print("Error, please enter numeric input")
-        break
+except:
+    print("Error, please enter numeric input")
